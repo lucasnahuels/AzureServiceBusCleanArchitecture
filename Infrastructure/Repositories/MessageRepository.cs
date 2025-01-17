ce5d659cb008c2760dcac01fd9569d56932b774c
lucasnahuels <lucasnahuelstp@gmail.com>
2025-01-17 01:19:55 -0300

    comments

--- Infrastructure/Repositories/MessageRepository.cs
+++ Infrastructure/Repositories/MessageRepository.cs
@@ -17,8 +17,11 @@ namespace Infrastructure.Repositories
 
         public async Task AddMessageAsync(Message message)
         {
+            //instantiating the message queue as a sender
             await using var client = new ServiceBusClient(_connectionString);
             ServiceBusSender sender = client.CreateSender(_queueName);
+
+            //sending a message to the queue    
             ServiceBusMessage serviceBusMessage = new ServiceBusMessage(message.Content);
             await sender.SendMessageAsync(serviceBusMessage);
         }
@@ -26,13 +29,16 @@ namespace Infrastructure.Repositories
         public async Task<IEnumerable<Message>> GetMessagesAsync()
         {
             var messages = new List<Message>();
+            //instantiating the message queue as a receiver
             await using var client = new ServiceBusClient(_connectionString);
             ServiceBusReceiver receiver = client.CreateReceiver(_queueName);
 
+            //getting the messages from the queue   
             var receivedMessages = await receiver.ReceiveMessagesAsync(maxMessages: 10);
             foreach (var receivedMessage in receivedMessages)
             {
                 messages.Add(new Message { Content = receivedMessage.Body.ToString() });
+                //deleting the message from the queue   
                 await receiver.CompleteMessageAsync(receivedMessage);
             }
 
